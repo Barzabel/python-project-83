9819d047ca7a96b48e3f3d86eee0d8270c965caf
Barzabek <kochenko1995@mail.ru>
2023-11-12 22:10:04 +0300

    add test

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.get('/')
 def index():
-    return "hi"
+    return render_template('index.html')
--- tests/test_first.py
+++ tests/test_first.py
@@ -1,5 +1,21 @@
 import pytest
+from page_analyzer.app import app as application
+
+@pytest.fixture()
+def flask_app():
+    app = application
+    yield app
+
+@pytest.fixture()
+def client(flask_app):
+    return flask_app.test_client()
 
 
-def test():
-    assert True
+@pytest.fixture()
+def runner(flask_app):
+    return flask_app.test_cli_runner()
+
+
+def test_request_example(client):
+    response = client.get("/")
+    assert b"Welcome to my awesome homepage." in response.data
