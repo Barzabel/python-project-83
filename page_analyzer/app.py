f6f54f50f52e32c7f348fd2e4ef209f74bac59b7
Barzabek <kochenko1995@mail.ru>
2023-11-13 07:25:18 +0300

    load env

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask import render_template
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
 
