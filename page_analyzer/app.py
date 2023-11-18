7c3c5b6bb5b77468112307a8396660580529a05c
Barzabek <kochenko1995@mail.ru>
2023-11-18 14:23:07 +0300

    add irls_chec

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,7 +9,7 @@ from flask import (
     abort,
 )
 from dotenv import load_dotenv
-from .db import Database
+from .db import Database_url, Database_url_checks
 from .validation import is_validat_url
 
 import os
@@ -32,28 +32,28 @@ def index():
 @app.post('/urls')
 def urls_create():
 
-    db = Database(DATABASE_URL)
+    db_url = Database_url(DATABASE_URL)
     url = request.form.to_dict()['url'].strip()
     errors = is_validat_url(url)
     if errors:
         flash('Некорректный URL', 'danger')
         messages = get_flashed_messages(with_categories=True)
         return render_template('index.html', url=url, messages=messages)
-    url_by_name = db.get_url_by_name(url)
+    url_by_name = db_url.get_url_by_name(url)
 
     if url_by_name:
         flash('Страница уже существует',  'info')
         return redirect(url_for('urls'))
     else:
-        db.add_url(url)
+        db_url.add_url(url)
         flash('Страница успешно добавлена', 'success')
         return redirect(url_for('urls'))
 
 
 @app.get('/urls')
 def urls():
-    db = Database(DATABASE_URL)
-    urls = db.get_urls()
+    db_url = Database_url(DATABASE_URL)
+    urls = db_url.get_urls()
     messages = get_flashed_messages(with_categories=True)
     return render_template('urls.html', urls=urls, messages=messages)
 
@@ -61,12 +61,25 @@ def urls():
 @app.get('/urls/<int:id>')
 def url(id):
     messages = get_flashed_messages(with_categories=True)
-    db = Database(DATABASE_URL)
-    url = db.get_url(id)
-
+    db_url = Database_url(DATABASE_URL)
+    url = db_url.get_url(id)
     if not url:
         return abort(404)
-    return render_template('url.html', url=url[0], messages=messages)
+    db_url_checks = Database_url_checks(DATABASE_URL)
+    url_checks = db_url_checks.get_url(id)
+    return render_template(
+        'url.html',
+        url=url[0],
+        url_checks=url_checks,
+        messages=messages
+    )
+
+
+@app.post('/urls/<int:id>/checks')
+def url_checks(id):
+    db_url_checks = Database_url_checks(DATABASE_URL)
+    db_url_checks.add_url_checks(id)
+    return redirect(url_for('url', id=id))
 
 
 @app.errorhandler(404)
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -7,7 +7,7 @@ class DatabaseException(Exception):
         self.message = message
 
 
-class Database:
+class Database_url:
     def __init__(self, config):
         try:
             self.conect = psycopg2.connect(config)
@@ -35,7 +35,12 @@ class Database:
     def get_urls(self):
         try:
             cur = self.conect.cursor(cursor_factory=NamedTupleCursor)
-            SQL = 'SELECT * FROM urls ORDER BY created_at DESC'
+            SQL = 'SELECT urls.id, urls.name, urls.created_at, \
+            urls_checks.status_code, MAX(urls_checks.created_at) FROM urls\
+            LEFT JOIN urls_checks ON urls.id=urls_checks.url_id GROUP BY \
+            urls.id, urls.name, urls.created_at, urls_checks.status_code, \
+            urls_checks.created_at ORDER BY urls.created_at DESC'
+
             cur.execute(SQL)
             result = cur.fetchall()
             return result
@@ -59,6 +64,43 @@ class Database:
             cur = self.conect.cursor(cursor_factory=NamedTupleCursor)
             SQL = 'SELECT * FROM urls WHERE name = (%s)'
             cur.execute(SQL, (name,))
+            result = cur.fetchall()
+            return result
+        except psycopg2.Error as e:
+            message = f'Can\'t get url from database! Error: {e}'
+            raise DatabaseException(message)
+
+
+class Database_url_checks:
+    def __init__(self, config):
+        try:
+            self.conect = psycopg2.connect(config)
+        except psycopg2.Error as e:
+            message = f'Can\'t connect to the database! Error: {e}'
+            raise DatabaseException(message)
+
+    def __del__(self):
+        self.conect.close()
+
+    def add_url_checks(self, url_id):
+        try:
+            cur = self.conect.cursor(cursor_factory=NamedTupleCursor)
+            SQL = 'INSERT INTO urls_checks (url_id) VALUES (%s) RETURNING id'
+            cur.execute(
+                    SQL,
+                    (str(url_id),))
+            result = cur.fetchall()
+            self.conect.commit()
+            return result
+        except psycopg2.Error as e:
+            message = f'Can\'t add url to database! Error: {e}'
+            raise DatabaseException(message)
+
+    def get_url(self, id):
+        try:
+            cur = self.conect.cursor(cursor_factory=NamedTupleCursor)
+            SQL = 'SELECT * FROM urls_checks WHERE url_id = (%s)'
+            cur.execute(SQL, (id,))
             result = cur.fetchall()
             return result
         except psycopg2.Error as e:
